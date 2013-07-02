bfeef009413483df6a6dfa4c21f77c2a5598ee8e
Michael McKelvaney <michaelmckelvaney@gmail.com>
2013-07-02 21:30:24 +0100

    serialmonitor.rb : tidied up the script and added some comments

--- serialmonitor.rb
+++ serialmonitor.rb
@@ -9,18 +9,20 @@ EOF
   exit(1)
 end
 
+# set up the log object - make sure the log folder is writeable
 log = Logger.new('/var/log/hamsterometer/hamsterometer.log')
 log.level = Logger::INFO
 
+# set up the serial port object, with baud rate of 9600
 sp = SerialPort.new(ARGV[0], 9600, 8, 1, SerialPort::NONE)
-
-
 
 # recieve part
 Thread.new do
   while TRUE do
     while (i = sp.gets) do
+      # output to STDOUT
       puts i
+      # output to log
       log.info(i.strip)
     end
   end 
