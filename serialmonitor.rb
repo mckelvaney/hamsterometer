e352da452f611cad37673b9f1776171a4d163754
Michael McKelvaney <michaelmckelvaney@gmail.com>
2013-07-02 21:22:37 +0100

    serialmonitor.rb : added in a logger object to output the contents of the serial line

--- serialmonitor.rb
+++ serialmonitor.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/env ruby
 require 'serialport'
+require 'logger'
+
 if ARGV.size < 1
   STDERR.print <<EOF
   Usage: #{$0} serial_port
 EOF
   exit(1)
 end
+
+log = Logger.new('/var/log/hamsterometer/hamsterometer.log')
+log.level = Logger::WARN
 
 sp = SerialPort.new(ARGV[0], 9600, 8, 1, SerialPort::NONE)
 
@@ -16,6 +21,7 @@ Thread.new do
   while TRUE do
     while (i = sp.gets) do
       puts Time.now.to_s + ' ' + i
+      logger.info(Time.now.to_s + ' ' + i)
     end
   end 
 end
