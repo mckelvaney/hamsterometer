2a5189b33e9a882ae70c5ac93d70db2405975a73
Michael McKelvaney <michael@mckelvaney.co.uk>
2013-07-03 21:55:10 +0100

    serialmonitor.rb : redirecting outputs with append, instead of overwriting. added a flush on stdout so that it is not buffered

--- serialmonitor.rb
+++ serialmonitor.rb
@@ -2,7 +2,6 @@
 require 'serialport'
 require 'logger'
 
-
 if ARGV.size < 1
   STDERR.print <<EOF
  Usage: #{$0} serial_port debug
@@ -16,8 +15,8 @@ debug = ARGV[1] || false
 Dir.mkdir('log/') unless Dir.exists?('log/')
 
 # redirect STDOUT and STDERR so that we can make this a daemon
-$stdout.reopen("log/out.log", "w")
-$stderr.reopen("log/error.log", "w")
+$stdout.reopen("log/out.log", "a")
+$stderr.reopen("log/error.log", "a")
 
 # set up the log object - make sure the log folder is writeable
 log = Logger.new('log/hamsterometer.log', 'daily')
@@ -26,22 +25,37 @@ log.level = Logger::INFO
 # set up the serial port object, with baud rate of 9600
 sp = SerialPort.new(ARGV[0], 9600, 8, 1, SerialPort::NONE)
 
+puts "STARTING"
+$stdout.flush
+
 # recieve part
 while TRUE do
 	while (i = sp.gets) do
 		# output to STDOUT
 		puts i if debug
+		$stdout.flush
 		# output to log
 		log.info(i.strip)
 	end
-end 
+end
+
+Signal.trap("SIGINT") do
+	sp.close
+	puts "Exiting"
+	$stdout.flush
+	exit
+end
 
 Signal.trap("TERM") do
 	sp.close
-	puts Exiting
+	puts "Exiting"
+	$stdout.flush
+	exit
 end
 
 Signal.trap("KILL") do
 	sp.close
-	puts Exiting
+	puts "Exiting"
+	$stdout.flush
+	exit
 end
